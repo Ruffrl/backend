8154eb099aba0432924d617d2b6ae5c41e982420
Priya Power <49959312+priyapower@users.noreply.github.com>
2024-07-25 21:49:49 -0500

    Setup some basic endpoints for testing request/response cycle and authentication endpoints

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,34 +1,24 @@
 # frozen_string_literal: true
 
-# Handles Devise registrations for sign-ins and sign-ups
-class Users::RegistrationsController < Devise::RegistrationsController
-  # For API development; will prevent rendering view
-  respond_to :json
+module Users
+  # Handles Devise registrations for sign-ins and sign-ups
+  class RegistrationsController < Devise::RegistrationsController
+    # For API development; will prevent rendering view
+    respond_to :json
 
-  private
+    private
 
-  # Test a POST request
-  # https://localhost:3000/signup
-  # BODY (raw)
-  # {
-  #   "user": {
-  #     "email": "test@test.com",
-  #     "password": "123456",
-  #     "forename": "Blarg",
-  #     "surname": "McBlargson",
-  #   }
-  # }
-
-  def respond_with(current_user, _opts = {})
-    if resource.persisted?
-      render json: {
-        status: { code: 200, message: 'Signed up successfully.' },
-        data: UserSerializer.new(current_user).serializable_hash[:data][:attributes]
-      }
-    else
-      render json: {
-        status: { message: "User couldn't be created successfully. #{current_user.errors.full_messages.to_sentence}" }
-      }, status: :unprocessable_entity
+    def respond_with(current_user, _opts = {})
+      if resource.persisted?
+        render json: {
+          status: { code: 200, message: 'Signed up successfully.' },
+          data: UserSerializer.new(current_user).serializable_hash[:data][:attributes]
+        }
+      else
+        render json: {
+          status: { message: "User couldn't be created successfully. #{current_user.errors.full_messages.to_sentence}" }
+        }, status: :unprocessable_entity
+      end
     end
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file should ensure the existence of records required to run the application in every environment (production,
 # development, test). The code here should be idempotent so that it can be executed at any point in every environment.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
@@ -14,10 +16,13 @@ User.destroy_all
 ActiveRecord::Base.connection.reset_pk_sequence!('users')
 puts "User count: #{User.all.count}"
 puts 'creating User records'
-User.create!(email: 'rock@gmail.com', password: 'password', forename: 'Sam', surname: 'Thomas', species: Constants.user.species.OWNER, jti: 'token1')
-User.create!(email: 'pop@hotmail.com', password: 'password', forename: 'Sarah', surname: 'Jones', species: Constants.user.species.CARETAKER, jti: 'token2')
-User.create!(email: 'country@msn.com', password: 'password', forename: 'Joe', surname: 'Smith', species: Constants.user.species.OWNER_AND_CARETAKER, jti: 'token3')
-User.create!(email: 'folk@aol.com', password: 'password', forename: 'Jen', surname: 'Adams', species:Constants.user.species.OWNER, jti: 'token4')
+User.create!(email: 'rock@gmail.com', password: 'password', forename: 'Sam', surname: 'Thomas',
+             species: Constants.user.species.OWNER, jti: 'token1')
+User.create!(email: 'pop@hotmail.com', password: 'password', forename: 'Sarah', surname: 'Jones',
+             species: Constants.user.species.CARETAKER, jti: 'token2')
+User.create!(email: 'country@msn.com', password: 'password', forename: 'Joe', surname: 'Smith',
+             species: Constants.user.species.OWNER_AND_CARETAKER, jti: 'token3')
+User.create!(email: 'folk@aol.com', password: 'password', forename: 'Jen', surname: 'Adams',
+             species: Constants.user.species.OWNER, jti: 'token4')
 puts "User count: #{User.all.count}"
 # ***************
-# 
