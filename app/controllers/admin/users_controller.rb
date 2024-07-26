a6e99417c3e3f091108a71675cf768b0e2bf6854
Priya Power <49959312+priyapower@users.noreply.github.com>
2024-07-26 13:02:14 -0500

    Update with correct auth before actions

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -3,6 +3,8 @@
 module Admin
   # For Admin testing User purposes
   class UsersController < ApplicationController
+    before_action :authenticate_user!, except: %I[index]
+
     rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
     rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
 
