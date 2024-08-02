a4165500f65a7e96b9e76e0995dd337015ad8153
Priya Power <49959312+priyapower@users.noreply.github.com>
2024-08-02 09:43:12 -0500

    Cleanup readme routes and add admin user index

--- config/routes.rb
+++ config/routes.rb
@@ -18,13 +18,12 @@ Rails.application.routes.draw do
     resource :email_verification, only: %i[show create]
     resource :password_reset,     only: %i[new edit create update]
   end
-  # resources :accounts, except: %i[index]
 
   namespace :admin do
     # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
     # Can be used by load balancers and uptime monitors to verify that the app is live.
     get 'up' => '/rails/health#show', as: :rails_health_check
 
-    # resources :accounts, only: %i[index]
+    resources :users, only: %i[index]
   end
 end
